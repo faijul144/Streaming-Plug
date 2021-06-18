bd54fc417d1962feaa1b78b1307596b9afaa43e0
Faijul Islam Fuad <f.faijulislam144@gmail.com>
2021-06-18 17:01:53 +0600

    Advertise add v1

--- assets/js/add-list.js
+++ assets/js/add-list.js
@@ -1,17 +1,17 @@
 "use strict";
 let date = new Date();
-let rand =
-  Math.floor(Math.random() * 100) +
-  date.getDay() +
-  date.getMilliseconds() +
-  date.getSeconds() +
-  date.getHours();
 
 $(".add-btn").click(function (e) {
   e.preventDefault();
   let listContainer = $(this).parent().find(".inputed-list");
   let inputField = $(this).data("target");
-
+  let rand =
+    "S" +
+    Math.floor(Math.random() * 100) +
+    date.getDay() +
+    date.getMilliseconds() +
+    date.getSeconds() +
+    date.getHours();
   if ($(inputField).val().length == "") {
     alert("Please Input Data");
   } else {
@@ -32,6 +32,7 @@ $(".add-btn").click(function (e) {
 </div>
 </div>`;
     listContainer.append(listTemplate);
+    $(inputField).val("");
   }
 });
 
@@ -68,8 +69,16 @@ function updateCon(x) {
 
 //Multyinput
 let dataArray = [];
+
 $(".multy-btn").click(function (e) {
   e.preventDefault();
+  let randM =
+    "M" +
+    Math.floor(Math.random() * 100) +
+    date.getDay() +
+    date.getMilliseconds() +
+    date.getSeconds() +
+    date.getHours();
   let listContainer = $(this).parent().find(".inputed-list");
   let fieldsContainer = $(this).data("target");
   let inputFields = $(fieldsContainer).find("input");
@@ -78,34 +87,29 @@ $(".multy-btn").click(function (e) {
   let template;
 
   inputFields.each(function () {
-    if (!$(this).val() == "") {
-      dataArray.push($(this).val());
-    } else {
-      alert("Please Input Data");
-    }
+    dataArray.push($(this).val());
+    $(this).val("");
   });
   selectFields.each(function () {
-    if (!$(this).find(":selected").val() == "") {
-      dataArray.push($(this).find(":selected").val());
-    } else {
-      alert("Please Input Data");
-    }
+    dataArray.push($(this).find(":selected").val());
+    $(this).find(":selected").attr("selected", "false");
   });
 
-  let companyTemp = `<div id=${rand}>
+  let companyTemp = `<div id=${randM}>
   <div>
       <div class="data">${dataArray[0]}</div>
-      <div class="data">${dataArray[2]}, ${dataArray[1]}</div>
+      <div class="data">${dataArray[1]}</div>
+      <div class="data">${dataArray[2]}</div>
       <div class="data">From ${dataArray[3]} - ${dataArray[4]}</div>
   </div>
   <div class="sm-action">
       <button class="sm-action-btn" data-action="edit"
-          onclick="editMulCon('${rand}')">
+          onclick="editMulCon('${randM}')">
           <i class="far fa-pencil"></i>
       </button>
       <button class="sm-action-btn"
           data-action="remove"
-          onclick="deleteCon('${rand}')">
+          onclick="deleteCon('${randM}')">
           <i class="far fa-times"></i>
       </button>
   </div>
@@ -120,17 +124,80 @@ function editMulCon(x) {
   let thisBtn = $("#" + x);
   let currentValue = [];
   let currentValues = thisBtn.find(".data");
+  let formSelectOpt = [];
+  let toSelectOpt = [];
+  thisBtn
+    .parent()
+    .parent()
+    .find(".from-select")
+    .find("option")
+    .each(function () {
+      formSelectOpt.push($(this).clone()[0].outerHTML);
+    });
+
+  thisBtn
+    .parent()
+    .parent()
+    .find(".to-select")
+    .find("option")
+    .each(function () {
+      toSelectOpt.push($(this).clone()[0].outerHTML);
+    });
 
   currentValues.each(function () {
-    if ($(this).text().includes(",")) {
-      currentValue.push($(this).text().split(","));
-    } else {
-      currentValue.push($(this).text());
-    }
+    currentValue.push($(this).text());
   });
-  alert(currentValue);
+
+  let fromI = 0;
+  let fromL = formSelectOpt.length;
+  var i;
+  let fullTime = currentValue[3].split(/[^A-Z0-9]/gi);
+
+  for (let j = 0; j < fullTime.length; j++) {
+    if (fullTime[j] == "" || fullTime[j] == "From") {
+      fullTime.splice(j, 1);
+    }
+  }
+  console.log(fullTime);
+
+  for (i = fromI; i < fromL; i++) {
+    if (formSelectOpt[i].includes(currentValue[3])) {
+      formSelectOpt[
+        i
+      ] = `<option value="${currentValue[3]}" selected="selected">${currentValue[2]}</option>`;
+    }
+    formSelectOpt[i] = formSelectOpt[i];
+  }
+
+  console.log(formSelectOpt);
 
   thisBtn.empty();
-  thisBtn.append(`<input type="text" class="form-control" value="${currentValue}">
-<button type="button" class="thm_btn br_btn btn_sub_cus_edit" onclick="updateCon('${x}')">Update</button>`);
+  thisBtn.addClass("d-block");
+  thisBtn.append(
+    `
+    <input type="text" class="form-control mb-2 w-100"
+        id="com-name" value="${currentValue[0]}">
+    <input type="text" class="form-control mb-2 w-100"
+        id="com-add" value="${currentValue[1]}">
+    <input type="text" class="form-control mb-2 w-100"
+        id="possition" value="${currentValue[2]}">
+    <div class="form-row w-100">
+        <div class="col-md-6">
+            <select name="from" class="form-control from-inner-select">` +
+      jQuery.each(formSelectOpt, function (i, val) {
+        val;
+      }) +
+      `</select>
+        </div>
+        <div class="col-md-6">
+            <select name="from" class="form-control to-inner-select">
+                <option value="Present">Present</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+            </select>
+        </div>
+    </div>
+  `
+  );
 }
