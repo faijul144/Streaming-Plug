5471e5fdb64a840136605d0377934cdafe8bcf01
Faijul Islam Fuad <f.faijulislam144@gmail.com>
2021-06-18 18:48:35 +0600

    Advertise add v2

--- assets/js/add-list.js
+++ assets/js/add-list.js
@@ -68,9 +68,9 @@ function updateCon(x) {
 }
 
 //Multyinput
-let dataArray = [];
 
 $(".multy-btn").click(function (e) {
+  let dataArray = [];
   e.preventDefault();
   let randM =
     "M" +
@@ -104,7 +104,7 @@ $(".multy-btn").click(function (e) {
   </div>
   <div class="sm-action">
       <button class="sm-action-btn" data-action="edit"
-          onclick="editMulCon('${randM}')">
+          onclick="editMulCon('${randM}','${addFor}')">
           <i class="far fa-pencil"></i>
       </button>
       <button class="sm-action-btn"
@@ -114,13 +114,30 @@ $(".multy-btn").click(function (e) {
       </button>
   </div>
 </div>`;
-  let instituteTemp;
+  let instituteTemp = `<div id=${randM}>
+  <div>
+      <div class="data">${dataArray[0]}</div>
+      <div class="data">${dataArray[1]}</div>
+      <div class="data">From ${dataArray[2]} - ${dataArray[3]}</div>
+  </div>
+  <div class="sm-action">
+      <button class="sm-action-btn" data-action="edit"
+          onclick="editMulCon('${randM},${addFor}')">
+          <i class="far fa-pencil"></i>
+      </button>
+      <button class="sm-action-btn"
+          data-action="remove"
+          onclick="deleteCon('${randM}')">
+          <i class="far fa-times"></i>
+      </button>
+  </div>
+</div>`;
   addFor == "company" ? (template = companyTemp) : (template = instituteTemp);
 
   listContainer.append(template);
 });
 
-function editMulCon(x) {
+function editMulCon(x, addedfor) {
   let thisBtn = $("#" + x);
   let currentValue = [];
   let currentValues = thisBtn.find(".data");
@@ -150,54 +167,149 @@ function editMulCon(x) {
 
   let fromI = 0;
   let fromL = formSelectOpt.length;
-  var i;
-  let fullTime = currentValue[3].split(/[^A-Z0-9]/gi);
-
-  for (let j = 0; j < fullTime.length; j++) {
-    if (fullTime[j] == "" || fullTime[j] == "From") {
-      fullTime.splice(j, 1);
-    }
-  }
-  console.log(fullTime);
+  var i, j;
+  let fullTime;
+  addedfor == "company"
+    ? (fullTime = currentValue[3].split(/\From|[^A-Z0-9]/gi).clean(""))
+    : (fullTime = currentValue[2].split(/\From|[^A-Z0-9]/gi).clean(""));
 
   for (i = fromI; i < fromL; i++) {
-    if (formSelectOpt[i].includes(currentValue[3])) {
+    if (formSelectOpt[i].includes(fullTime[0])) {
       formSelectOpt[
         i
-      ] = `<option value="${currentValue[3]}" selected="selected">${currentValue[2]}</option>`;
+      ] = `<option value="${fullTime[0]}" selected="selected">${fullTime[0]}</option>`;
     }
     formSelectOpt[i] = formSelectOpt[i];
   }
 
-  console.log(formSelectOpt);
+  for (j = fromI; j < fromL; j++) {
+    if (toSelectOpt[j].includes(fullTime[1])) {
+      toSelectOpt[
+        j
+      ] = `<option value="${fullTime[1]}" selected="selected">${fullTime[1]}</option>`;
+    }
+    toSelectOpt[j] = toSelectOpt[j];
+  }
+
+  let template;
+  let comTemplate =
+    `
+<input type="text" class="form-control mb-2 w-100"
+    id="com-name" value="${currentValue[0]}">
+<input type="text" class="form-control mb-2 w-100"
+    id="com-add" value="${currentValue[1]}">
+<input type="text" class="form-control mb-2 w-100"
+    id="possition" value="${currentValue[2]}">
+<div class="form-row w-100">
+    <div class="col-md-6">
+        <select name="from" class="form-control from-inner-select">` +
+    jQuery.each(formSelectOpt, function (i, val) {
+      val;
+    }) +
+    `</select>
+    </div>
+    <div class="col-md-6">
+        <select name="from" class="form-control to-inner-select">` +
+    jQuery.each(toSelectOpt, function (i, val) {
+      val;
+    }) +
+    `</select>
+    </div>
+    <button type="button" onclick="multiUpdate('${x}','${addedfor}')" class="thm_btn br_btn btn_sub_cus_md mt-3">Update</button>
+</div>
+`;
+  let insTemplate =
+    `
+  <input type="text" class="form-control mb-2 w-100" value="${currentValue[0]}">
+  <input type="text" class="form-control mb-2 w-100" value="${currentValue[1]}">
+  <div class="form-row w-100">
+      <div class="col-md-6">
+          <select name="from" class="form-control from-inner-select">` +
+    jQuery.each(formSelectOpt, function (i, val) {
+      val;
+    }) +
+    `</select>
+      </div>
+      <div class="col-md-6">
+          <select name="from" class="form-control to-inner-select">` +
+    jQuery.each(toSelectOpt, function (i, val) {
+      val;
+    }) +
+    `</select>
+      </div>
+      <button type="button" onclick="multiUpdate('${x}','${addedfor}')" class="thm_btn br_btn btn_sub_cus_md mt-3">Update</button>
+  </div>
+  `;
+
+  addedfor == "company" ? (template = comTemplate) : (template = insTemplate);
 
   thisBtn.empty();
   thisBtn.addClass("d-block");
-  thisBtn.append(
-    `
-    <input type="text" class="form-control mb-2 w-100"
-        id="com-name" value="${currentValue[0]}">
-    <input type="text" class="form-control mb-2 w-100"
-        id="com-add" value="${currentValue[1]}">
-    <input type="text" class="form-control mb-2 w-100"
-        id="possition" value="${currentValue[2]}">
-    <div class="form-row w-100">
-        <div class="col-md-6">
-            <select name="from" class="form-control from-inner-select">` +
-      jQuery.each(formSelectOpt, function (i, val) {
-        val;
-      }) +
-      `</select>
-        </div>
-        <div class="col-md-6">
-            <select name="from" class="form-control to-inner-select">
-                <option value="Present">Present</option>
-                <option value="2021">2021</option>
-                <option value="2020">2020</option>
-                <option value="2019">2019</option>
-            </select>
-        </div>
-    </div>
-  `
-  );
+
+  thisBtn.append(template);
 }
+
+function multiUpdate(x, addedfor) {
+  let updateCon = $("#" + x);
+  let inputFields = updateCon.find("input");
+  let selectFields = updateCon.find("select");
+  let template;
+  let newDataArray = [];
+
+  inputFields.each(function () {
+    newDataArray.push($(this).val());
+    $(this).val("");
+  });
+  selectFields.each(function () {
+    newDataArray.push($(this).find(":selected").val());
+    $(this).find(":selected").attr("selected", "false");
+  });
+
+  let companyTemp = `<div>
+      <div class="data">${newDataArray[0]}</div>
+      <div class="data">${newDataArray[1]}</div>
+      <div class="data">${newDataArray[2]}</div>
+      <div class="data">From ${newDataArray[3]} - ${newDataArray[4]}</div>
+  </div>
+  <div class="sm-action">
+      <button class="sm-action-btn" data-action="edit"
+          onclick="editMulCon('${x}','${addedfor}')">
+          <i class="far fa-pencil"></i>
+      </button>
+      <button class="sm-action-btn"
+          data-action="remove"
+          onclick="deleteCon('${x}')">
+          <i class="far fa-times"></i>
+      </button>
+  </div>`;
+  let instituteTemp = `<div>
+  <div class="data">${newDataArray[0]}</div>
+  <div class="data">${newDataArray[1]}</div>
+  <div class="data">From ${newDataArray[2]} - ${newDataArray[3]}</div>
+</div>
+<div class="sm-action">
+  <button class="sm-action-btn" data-action="edit"
+      onclick="editMulCon('${x}','${addedfor}')">
+      <i class="far fa-pencil"></i>
+  </button>
+  <button class="sm-action-btn"
+      data-action="remove"
+      onclick="deleteCon('${x}')">
+      <i class="far fa-times"></i>
+  </button>
+</div>`;
+  addedfor == "company" ? (template = companyTemp) : (template = instituteTemp);
+  updateCon.empty();
+  updateCon.removeClass("d-block");
+  updateCon.append(template);
+}
+
+Array.prototype.clean = function (deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
