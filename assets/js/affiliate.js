c4b9d34389cb017385e99441202be10406500f25
Faijul Islam Fuad <f.faijulislam144@gmail.com>
2021-06-20 14:07:10 +0600

    Dark mode edits-minor v1

--- assets/js/affiliate.js
+++ assets/js/affiliate.js
@@ -46,89 +46,120 @@ $(".aff-link>label").on("click", function (e) {
   $temp.remove();
 });
 
-// Hight Chart
-var chart = Highcharts.stockChart("newChart", {
-  xAxis: {
-    ordinal: false,
-  },
-
-  plotOptions: {
-    series: {
-      dataGrouping: {
-        forced: true,
-        approximation: "sum",
-      },
-    },
-  },
-
-  rangeSelector: {
-    enabled: false,
-  },
-
-  series: [
+// Chart Js
+var ctx = document.getElementById("newChart").getContext("2d");
+const Yearlylabels = ["2016", "2017", "2018", "2019", "2020", "2021"];
+const Monthlylabels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const Weeklylabels = [
+  "21-6-21",
+  "22-6-21",
+  "23-6-21",
+  "24-6-21",
+  "25-6-21",
+  "26-6-21",
+  "27-6-21",
+];
+const Dailylylabels = [
+  "21-6-21",
+  "22-6-21",
+  "23-6-21",
+  "24-6-21",
+  "25-6-21",
+  "26-6-21",
+  "27-6-21",
+  "28-6-21",
+  "29-6-21",
+  "30-6-21",
+];
+const Yeardata = {
+  labels: Yearlylabels,
+  datasets: [
     {
-      data: [
-        [Date.UTC(2020, 0, 1), 9],
-        [Date.UTC(2021, 0, 2), 3],
-        [Date.UTC(2020, 3, 1), 1],
-        [Date.UTC(2021, 4, 1), 7],
-      ],
+      label: "Yearly Data",
+      backgroundColor: "#ff9100",
+      borderColor: "#ff9100",
+      data: [5, 10, 5, 2, 20, 30],
     },
   ],
-});
+};
 
+const Monthdata = {
+  labels: Monthlylabels,
+  datasets: [
+    {
+      label: "Monthly Data",
+      backgroundColor: "#ff9100",
+      borderColor: "#ff9100",
+      data: [5, 10, 5, 2, 20, 30, 45, 12, 55, 82, 32, 56],
+    },
+  ],
+};
+
+const Weekdata = {
+  labels: Weeklylabels,
+  datasets: [
+    {
+      label: "Weekly Data",
+      backgroundColor: "#ff9100",
+      borderColor: "#ff9100",
+      data: [5, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
+const Dailydata = {
+  labels: Dailylylabels,
+  datasets: [
+    {
+      label: "Daily Data",
+      backgroundColor: "#ff9100",
+      borderColor: "#ff9100",
+      data: [5, 10, 5, 2, 20, 30, 45, 12, 55, 82],
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: Dailydata,
+  options: {},
+};
+var chart = new Chart(ctx, config);
+
+//Data Change By Buttons
 $(".chart-view-types li button").each(function () {
   var type = $(this).data("type");
   $("#ch-" + type).click(function () {
     $(".chart-view-types li button").removeClass("thm_btn-black");
     $(this).addClass("thm_btn-black");
-    chart.update({
-      plotOptions: {
-        series: {
-          dataGrouping: {
-            units: [[type, [0]]],
-          },
-        },
-      },
-    });
+    if (type == "day") {
+      chart.data = Dailydata;
+      chart.update();
+    }
+    if (type == "month") {
+      chart.data = Monthdata;
+      chart.update();
+    }
+    if (type == "week") {
+      chart.data = Weekdata;
+      chart.update();
+    }
+    if (type == "year") {
+      chart.data = Yeardata;
+      chart.update();
+    }
   });
 });
-
-// apex chart
-// var options = {
-//   series: [
-//     {
-//       name: "Desktops",
-//       data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
-//     },
-//   ],
-//   chart: {
-//     height: 350,
-//     type: "line",
-//     zoom: {
-//       enabled: false,
-//     },
-//   },
-//   dataLabels: {
-//     enabled: false,
-//   },
-//   stroke: {
-//     curve: "straight",
-//   },
-//   title: {
-//     text: "Summary",
-//     align: "left",
-//   },
-//   grid: {
-//     row: {
-//       colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-//       opacity: 0.5,
-//     },
-//   },
-//   xaxis: {
-//     categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
-//   },
-// };
-
-// var chart = new ApexCharts(document.querySelector("#newChart"), options);
-// chart.render();
